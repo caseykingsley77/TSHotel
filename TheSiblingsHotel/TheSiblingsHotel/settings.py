e4a25fa7139177c74cad877b5a6100a3575f1e42
Caseykingsley77 <caseykingsley77@gmail.com>
2024-10-26 19:57:11 +0100

    settings

--- TheSiblingsHotel/TheSiblingsHotel/settings.py
+++ TheSiblingsHotel/TheSiblingsHotel/settings.py
@@ -17,7 +17,7 @@ import environ
 env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(os.path.dirname(__file__), r'C:\Users\Olix\TSH\TheSiblingsHotel\.env'))
 
 
 # Quick-start development settings - unsuitable for production
